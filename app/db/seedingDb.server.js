e88c245ba4b5aa75550e4a91e11ecca2b739e23b
Dan Okkels Brendstrup <dob@eaaa.dk>
2024-02-21 12:19:03 +0100

    feat: more recent dates for entries

--- app/db/seedingDb.server.js
+++ app/db/seedingDb.server.js
@@ -11,27 +11,27 @@ export default async function seedDb() {
 async function insertData() {
   const entries = [
     {
-      date: new Date("2021-01-01"),
+      date: new Date("2024-01-01"),
       type: "work",
       text: "I'm working",
     },
     {
-      date: new Date("2021-01-02"),
+      date: new Date("2024-01-15"),
       type: "learning",
       text: "I'm learning",
     },
     {
-      date: new Date("2021-01-03"),
+      date: new Date("2024-02-01"),
       type: "interesting-thing",
       text: "I'm doing something interesting",
     },
     {
-      date: new Date("2021-02-19"),
+      date: new Date("2024-02-15"),
       type: "learning",
       text: "Remix Auth with FormStrategy and Post App",
     },
     {
-      date: new Date("2021-02-22"),
+      date: new Date("2024-02-22"),
       type: "work",
       text: "Remix Work Journal",
     },
